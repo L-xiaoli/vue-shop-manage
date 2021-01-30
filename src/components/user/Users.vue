18d86038e27d7fda4729e3ee18357f482f1f0d25
L-xiaoli <625529828@qq.com>
2021-01-30 11:56:31 +0800

    用户搜索功能

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -12,12 +12,12 @@
       <!-- 搜索与添加区域 -->
       <el-row :gutter="20">
         <el-col :span="7">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getUserList">
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary">添加用户</el-button>
+          <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
         </el-col>
       </el-row>
       <!-- 用户列表(表格)区域 -->
