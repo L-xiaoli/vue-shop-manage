ac238e76a99e61462cd4727b075c9ec61dc257c6
L-xiaoli <625529828@qq.com>
2021-01-30 20:57:59 +0800

    权限删除功能

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -24,7 +24,7 @@ if there's nested data, rowKey is required.
             <el-row :class="['bdbottom', i1 === 0 ? 'bdtop' : '', 'vcenter']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
               <!-- 渲染一级权限 -->
               <el-col :span="5">
-                <el-tag> {{ item1.authName }} </el-tag>
+                <el-tag closable @close="removeRightById(scope.row, item1.id)"> {{ item1.authName }} </el-tag>
                 <i class="el-icon-caret-right"></i>
               </el-col>
               <!-- 渲染二级 和 三级 权限 -->
@@ -32,12 +32,12 @@ if there's nested data, rowKey is required.
                 <!-- 通过for循环 嵌套渲染二级权限 -->
                 <el-row :class="[i2 === 0 ? '' : 'bdtop', 'vcenter']" v-for="(item2, i2) in item1.children" :key="item2.id">
                   <el-col :span="6">
-                    <el-tag type="success"> {{ item2.authName }} </el-tag>
+                    <el-tag type="success" closable @close="removeRightById(scope.row, item2.id)"> {{ item2.authName }} </el-tag>
                     <i class="el-icon-caret-right"></i>
                   </el-col>
                   <!-- 三级权限 -->
                   <el-col :span="18">
-                    <el-tag type="warning" :class="[i3 === 0 ? '' : 'bdtop']" v-for="(item3, i3) in item2.children" :key="item3.id"> {{ item3.authName }} </el-tag>
+                    <el-tag type="warning" :class="[i3 === 0 ? '' : 'bdtop']" v-for="(item3, i3) in item2.children" :key="item3.id" closable @close="removeRightById(scope.row, item3.id)"> {{ item3.authName }} </el-tag>
                   </el-col>
                 </el-row>
               </el-col>
@@ -79,6 +79,26 @@ export default {
       this.roleList = res.data
       console.log(res.data)
       //   this.roleList = res.data
+    },
+    //g根据id删除对应ID
+    async removeRightById(role, rightId) {
+      //弹框提示用户是否删除
+      const confirmResult = await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch((err) => err)
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('取消删除')
+      }
+      const { data: res } = await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除权限失败！')
+      }
+      this.$message.success('删除权限成功！')
+      //属性列表
+      //   this.getRoleList()
+      role.children = res.data
     }
   }
 }
