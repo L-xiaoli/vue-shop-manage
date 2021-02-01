07d89af3a50fa3d714e3995c3c8161b305fc6848
L-xiaoli <625529828@qq.com>
2021-02-01 22:22:08 +0800

    分类参数的头部基本结构

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -6,6 +6,15 @@
       <el-breadcrumb-item>商品管理</el-breadcrumb-item>
       <el-breadcrumb-item>参数列表</el-breadcrumb-item>
     </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 头部警告区域 -->
+      <!-- :closable="false" ：不可关闭 ；  show-icon ：显示图标-->
+      <el-alert title="注意：只允许为第三级分类设置相关参数" type="warning" :closable="false" show-icon></el-alert>
+      <el-row class="opt_params">
+        <el-col>选择商品的分类:</el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
@@ -21,4 +30,8 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.opt_params {
+  margin: 15px 0;
+}
+</style>
