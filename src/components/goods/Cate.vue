dedbf01f23bc500dcdb64323e21c459db3993038
L-xiaoli <625529828@qq.com>
2021-02-01 15:22:12 +0800

    添加分类成功

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -185,12 +185,24 @@ export default {
     },
     //添加分类
     addCate() {
-      console.log(this.addCateForm)
+      //   console.log(this.addCateForm)
+      this.$refs.addCateRef.validate(async (valid) => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('categories', this.addCateForm)
+        if (res.meta.status !== 201) {
+          return this.$message.error('添加分类失败！')
+        }
+        this.$message.success('添加分类成功！')
+        this.getCateList()
+        this.addDialogVisible = false
+      })
     },
     //关闭对话框
     addDialogClosed() {
       this.$refs.addCateRef.resetFields()
-      ;(this.selectedKeys = []), (this.addCateForm.cat_pid = 0), (this.addCateForm.cat_level = 0)
+      this.selectedKeys = []
+      this.addCateForm.cat_pid = 0
+      this.addCateForm.cat_level = 0
     }
   }
 }
