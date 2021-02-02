0f7310c5d1c59df742f29541c4116fe6fa6c86f7
L-xiaoli <625529828@qq.com>
2021-02-02 21:58:14 +0800

    渲染头部搜索和按钮的基本结构

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -6,6 +6,20 @@
       <el-breadcrumb-item>商品管理</el-breadcrumb-item>
       <el-breadcrumb-item>商品列表</el-breadcrumb-item>
     </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 搜索与添加区域 -->
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加商品</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
