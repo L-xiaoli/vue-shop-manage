3ba04dd6f109cee46d3337750b5ab7f82ffb9aff
L-xiaoli <625529828@qq.com>
2021-02-03 21:12:18 +0800

    获取订单数据

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    订单管理
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 搜索与添加区域 -->
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable>
+            <el-button slot="append" icon="el-icon-search" @click="getOrderList"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
@@ -9,10 +28,33 @@ export default {
   name: 'order',
 
   data() {
-    return {}
+    return {
+      //获取查询订单信息的参数
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      total: 0, //数据总条数,
+      orderList: '' //订单数据列表
+    }
   },
-
-  methods: {}
+  created() {
+    this.getOrderList()
+  },
+  methods: {
+    //获得订单数据列表
+    async getOrderList() {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取订单数据失败!')
+      }
+      this.total = res.data.total
+      this.orderList = res.data.goods
+    }
+  }
 }
 </script>
 
