cf6aee05943909c014f4df38407ac996aa257df6
L-xiaoli <625529828@qq.com>
2021-02-03 21:31:43 +0800

    渲染订单表格

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -19,6 +19,34 @@
           <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
         </el-col>
       </el-row>
+      <!-- 订单数据表格 -->
+      <el-row>
+        <el-table :data="orderList" border stripe>
+          <el-table-column type="index"></el-table-column>
+          <el-table-column label="订单编号" prop="order_number"></el-table-column>
+          <el-table-column label="订单价格" prop="order_price"></el-table-column>
+          <el-table-column label="是否付款" prop="pay_status">
+            <template slot-scope="scope">
+              <el-tag type="success" v-if="scope.row.pay_status === '1'">已付款</el-tag>
+              <el-tag type="danger" v-else>未付款</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="是否发货" prop="is_send"> </el-table-column>
+          <el-table-column label="下单时间" prop="create_time">
+            <template slot-scope="scope">
+              {{ scope.row.create_time | dateFormat }}
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <!-- 修改 -->
+              <el-button type="primary" icon="el-icon-edit" size="mini" @click="showOrderDialog(scope.row.id)" @close="editDialogClosed"></el-button>
+              <!-- 定位 -->
+              <el-button type="success" icon="el-icon-location" size="mini"></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-row>
     </el-card>
   </div>
 </template>
@@ -36,7 +64,7 @@ export default {
         pagesize: 10
       },
       total: 0, //数据总条数,
-      orderList: '' //订单数据列表
+      orderList: [] //订单数据列表
     }
   },
   created() {
@@ -53,7 +81,10 @@ export default {
       }
       this.total = res.data.total
       this.orderList = res.data.goods
-    }
+    },
+    //展示数据
+    showOrderDialog() {},
+    editDialogClosed() {}
   }
 }
 </script>
