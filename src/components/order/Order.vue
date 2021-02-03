e6bb2f3b3ed11289d338dabedff0ff6cb243528f
L-xiaoli <625529828@qq.com>
2021-02-03 20:45:36 +0800

    给订单列表配置路由

---
+++ src/components/order/Order.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    订单管理
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'order',
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
