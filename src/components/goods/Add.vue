eb8e363bdb5727ed81f77c1842e56d5c22733d6d
L-xiaoli <625529828@qq.com>
2021-02-03 10:42:53 +0800

    获取商品分类数据并渲染级联选择器

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -21,13 +21,37 @@
         <el-step title="完成"></el-step>
       </el-steps>
       <!-- Tab栏区域 -->
-      <el-tabs :tab-position="'left'" v-model="activeIndex" style="height: 200px;">
-        <el-tab-pane label="基本信息" name="0"> 基本信息</el-tab-pane>
-        <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
-        <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
-        <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
-        <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
-      </el-tabs>
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRulesRef" label-width="100px" label-position="top">
+        <el-tabs :tab-position="'left'" v-model="activeIndex">
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addForm.goods_price" type="number"></el-input>
+            </el-form-item>
+
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addForm.goods_weight" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addForm.goods_number" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类" prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+
+            <el-form-item label="商品分类" prop="goods_cat">
+              <!-- 选择商品分类的级联选择框 -->
+              <el-cascader expandTrigger="hover" v-model="addForm.goods_cat" :options="cateList" :props="cateProps" @change="handleChange" clearable></el-cascader>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -38,11 +62,58 @@ export default {
 
   data() {
     return {
-      activeIndex: '0'
+      activeIndex: '0',
+      //添加商品的表单数据
+      //用来保存分类数据
+      cateList: [],
+      addForm: {
+        goods_name: '',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0,
+        goods_cat: [] //商品所属分类的数组
+      },
+
+      // 加商品的表单数据验证规则
+      addFormRules: {
+        goods_name: [{ required: true, message: '请输入商品名称', trigger: 'blur' }],
+        goods_price: [{ required: true, message: '请输入商品价格', trigger: 'blur' }],
+        goods_weight: [{ required: true, message: '请输入商品重量', trigger: 'blur' }],
+        goods_number: [{ required: true, message: '请输入商品数量', trigger: 'blur' }],
+        goods_cat: [{ required: true, message: '请选择商品分类', trigger: 'blur' }]
+      },
+      //配置级联菜单中数据如何展示
+      // cateProps:
+      //   {
+      //     value: 'cat_id',
+      //     label: 'cat_name',
+      //     children: 'children',
+      //     expandTrigger: 'hover'
+      //   },
+      cateProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children',
+        expandTrigger: 'hover' //触发形式
+      }
     }
   },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类数据失败！')
+      }
+      this.$message.success('获取商品分类数据成功！')
+      this.cateList = res.data
 
-  methods: {}
+      console.log(this.cateList)
+    },
+    handleChange() {}
+  }
 }
 </script>
 
