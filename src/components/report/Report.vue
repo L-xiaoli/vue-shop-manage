2eb087acfcdf7306b09abb723f3f0def0d2e367d
L-xiaoli <625529828@qq.com>
2021-02-04 11:27:23 +0800

    配置路由和渲染基本结构

---
+++ src/components/report/Report.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      111
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
